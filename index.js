15d4ea3207c6c7f113c7fc15b5394444894584cc
thuanduy <zinzxc1997@gmail.com>
2021-02-03 17:40:57 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+/**
+ * @format
+ */
+
+import {name as appName} from './app.json';
+import {AppRegistry} from 'react-native';
+
+//import App from './App';
+//AppRegistry.registerComponent(appName, () => App);
+
+///////////////////////////////////////
+// import Bananas from './src/example/props/Banana';
+// AppRegistry.registerComponent(appName, () => Bananas);
+
+//import LotsOfGreetings from './src/example/props/LotsOfGreetings';
+// AppRegistry.registerComponent(appName, () => LotsOfGreetings);
+
+// import BlinkApp from "./src/example/states/BlinkApp";
+// AppRegistry.registerComponent(appName, () => BlinkApp);
+
+//import LotsOfStyles from "./src/example/style/LotsOfStyles";
+//AppRegistry.registerComponent(appName, () => LotsOfStyles);
+
+// import ButtonBasics from "./src/example/touches/ButtonBasics";
+// AppRegistry.registerComponent(appName, () => ButtonBasics);
+
+// import FlatListBasics from "./src/example/listviews/FlatListBasics";
+// AppRegistry.registerComponent(appName, () => FlatListBasics);
+
+import SectionListBasics from "./src/example/listviews/SectionListBasics";
+AppRegistry.registerComponent(appName, () => SectionListBasics);
